90bc01975e1d1f922f500410cb14d2171c88446c
LiamRalph <33609759+LiamRalph@users.noreply.github.com>
2022-05-24 22:40:09 -0400

    Update makeXYchartRound.js

--- public/makeXYchartRound.js
+++ public/makeXYchartRound.js
@@ -119,7 +119,7 @@ async function renderRound(){
 
     xAxis.children.push(
       am5.Label.new(root, {
-        text: "Tick",
+        text: "Time",
         x: am5.percent(52),
         centerX:am5.percent(50),
         fontSize: 40,
@@ -130,9 +130,11 @@ async function renderRound(){
 
     let colours = [ 0x0000FF, 0xff0000, 0x00FF00, 0x000000, 0xC90076, 0xBC4400, 0xBCA200]
 
+    function fmtMSS(s){return(s-(s%=60))/60+(9<s?':':':0')+s}
 
-    data = data.map(t => ({probabilitytick: (t.probabilitytick).toFixed(2), tick: t.tick, attacker: t.attacker.charAt(0).toUpperCase()+t.attacker.slice(1), victim: t.victim.charAt(0).toUpperCase()+t.victim.slice(1), damage: t.damage, probabilitychange: Math.abs((t.probabilitychange).toFixed(2)), XrWPA: Math.abs((t.probabilitychange*t.expectedkill).toFixed(2)), winner: t.winner.charAt(0).toUpperCase()+t.winner.slice(1), expectedkill: t.expectedkill, teammembersalive: t.teammembersalive, opponentsalive: t.opponentsalive}));
+    data = data.map(t => ({probabilitytick: (t.probabilitytick).toFixed(2), time: fmtMSS((t.tick/128).toFixed(0)), tick: t.tick, attacker: t.attacker.charAt(0).toUpperCase()+t.attacker.slice(1), victim: t.victim.charAt(0).toUpperCase()+t.victim.slice(1), damage: t.damage, probabilitychange: Math.abs((t.probabilitychange).toFixed(2)), XrWPA: Math.abs((t.probabilitychange*t.expectedkill).toFixed(2)), winner: t.winner.charAt(0).toUpperCase()+t.winner.slice(1), expectedkill: t.expectedkill, teammembersalive: t.teammembersalive, opponentsalive: t.opponentsalive}));
 
+    
 
     for(let i = 0; i < data.length; i++){
       if(data[i].expectedkill > 0){
@@ -160,7 +162,7 @@ async function renderRound(){
           sprite: am5.Circle.new(root, {
             radius: 5,
             fill: am5.color(0x000000),
-            tooltipText: "[bold] Tick {tick} - {attacker} kills {victim} - {teammembersalive} vs {opponentsalive} \n xKill {expectedkill} - rWPA {probabilitytick} ± {probabilitychange} - XrWPA {XrWPA} ",
+            tooltipText: "[bold] Tick {tick} - {time} - {attacker} kills {victim} - {teammembersalive} vs {opponentsalive} \n xKill {expectedkill} - rWPA {probabilitytick} ± {probabilitychange} - XrWPA {XrWPA} ",
             tooltip: am5.Tooltip.new(root, {
               scale: 0.4,
               
