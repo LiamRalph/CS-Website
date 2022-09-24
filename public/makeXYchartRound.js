77d8ebf19fe5ffc27d17b46281a3135a49798287
LiamRalph <33609759+LiamRalph@users.noreply.github.com>
2022-09-24 15:47:26 -0400

    Update makeXYchartRound.js

--- public/makeXYchartRound.js
+++ public/makeXYchartRound.js
@@ -169,7 +169,7 @@ async function renderRound(){
           sprite: am5.Circle.new(root, {
             radius: 5,
             fill: am5.color(0x000000),
-            tooltipText: "Tick {tick} - {time} - {teammembersalive} vs {opponentsalive} \n{attacker} kills {victim} - xKill {expectedkill} \nrWPA {probabilitytick} ± {probabilitychange} - XrWPA {XrWPA} ",
+            tooltipText: "Tick {tick} - {time} - {teammembersalive} vs {opponentsalive} \n{attacker} kills {victim} - xKill {expectedkill} \nRI {probabilitytick} ± {probabilitychange} - xRI {XrWPA} ",
             tooltip: am5.Tooltip.new(root, {
               scale: 0.4,
             })
